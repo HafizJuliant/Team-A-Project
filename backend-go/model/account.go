0968f1db6af0aab641947d4af3f9bd3169607ef9
ahmadhafis <ahmadhafis296@gmail.com>
2024-11-15 13:19:53 +0700

    add alamat account my

--- backend-go/model/account.go
+++ backend-go/model/account.go
@@ -4,6 +4,7 @@ type Account struct {
 	AccountID int64  `json:"account_id" gorm:"primaryKey;autoIncrement;<-:false"`
 	Name      string `json:"name"`
 	Balance   int64  `json:"balance"`
+	Alamat 		string `json:"alamat"`
 	No_telepon int64 `json:"no_telepon"`
 	Bank_id string `json:"bank_id"`
 }
